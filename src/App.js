5e74c87b3d6bb8cb99f00728bce3a042997e2e3a
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-07 13:10:33 -0400

    Bare comments about TDD

--- src/App.js
+++ src/App.js
@@ -14,4 +14,31 @@ export default App;
 
 ///Watch mode watches for changes an re renders and looks for the last commit 
 
-//
+//Enzyme vs ReactDOM
+//Enzyme has a better toolkit 
+ //Search thru the DOM 
+ //allows to click on node 
+
+
+ //Types of test 
+  //Unit testing - Test one piece of code (function)
+  //Integration - How multiple units work together 
+  //End to End - How a user interacts with the app 
+
+  //Testing goals 
+  //Test behavior not implemantion
+  // what the app should do instead of how it works 
+  //If implemantation changes test should still pass 
+  
+  //Example 
+  //clickCounter 
+
+  //TESTING BEHAVIOR :)
+   //Set initial state 
+   //Simulate button click
+   //Check displayed count to see that it increased 
+
+  //TESTING IMPLEMENTATION X 
+   //set initial state 
+   //simulate click
+   //check if the function was called 
