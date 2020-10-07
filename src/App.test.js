510f0ac8c257113cff643a373bc62687a36263ce
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-07 14:37:23 -0400

    Started on Basic clicker tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,27 @@ import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
-test('I expect this to be truthy', () => {
+test('Renders without errors', () => {
   const wrapper = shallow(<App />)
-  expect(wrapper).toBeTruthy()
-});
+  //You want to use data-test (kinda like id or className will check if it rendered)
+  //Why use new attribute? 
+  //id and class have uses in apps so it might be changed for 
+  //CSS or functionality 
+  //Plus it wont be used in production 
+})
+
+test('Renders Button', () => {
+  
+})
+
+test('Renders counter Display', () => {
+  
+})
+
+test('Counter starts at 0', () => {
+  
+})
+
+test('Clicking on a button increments counter', () => {
+  
+})
